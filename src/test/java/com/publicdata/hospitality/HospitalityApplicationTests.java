5d7ccdd5d3d07cd32b3a7ab35f02c4fab6d1aa4a
stackfish <app8229@knou.ac.kr>
2023-08-03 23:06:10 +0900

    init

---
+++ src/test/java/com/publicdata/hospitality/HospitalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.publicdata.hospitality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
